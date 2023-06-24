06f810516a9dcb1bf9c04c5a4438e9d213ebe8a4
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-06-24 19:20:36 +0800

    updated test code for network2

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 """
 import mnist_loader
 import network
+import network2
 
 # network.py example
 
@@ -20,3 +21,11 @@ import network
 training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
 net = network.Network([784, 30, 10])
 net.SGD(training_data , 30, 10, 3.0, test_data=test_data)
+
+
+#network2.py example
+training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
+net2 =network2.Network([784, 30, 10], cost= network2.CrossEntropyCost)
+net2.large_weight_initializer()
+net2.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,monitor_evaluation_accuracy=True)
+
--- network2.py
+++ network2.py
@@ -14,19 +14,26 @@ import numpy as np
 ##### Cross-entropy cost functions
 
 class CrossEntropyCost(object):
-
+    """ The Cross Entropy Cost Function is an improvement from the quadratic cost function in network.py. Unlike the quadratic cost function, 
+        the Cross-Entropy Cost Function avoids the problem of the learning slowing down because if the neuron's actual output is close to the desired
+        output for all training inputs, z, then the cross-entropy will be close to zero. The cross-entropy learns faster if the initial error is larger. 
+    """
     @staticmethod
     def fn(a, y):
         """ a measure of how well an output activation, a, matches its desired output, y.
             C = 1/n * ∑(Xj (σ(z)-7))
+            Return the cost associated with an output "a" and desired output "y" Np.nan_to_num ensure numerical stability. If both "a" and "y"
+            have a 1.0 in the same slot then the expresion (1-y)*np.log(1-a) returns nan. the np.nan_to_num rensures that this converted to the correct 
+            value of (0.0).
         """
-
         # np.nan_to_num ensures that the np deals correctly with the log of numbers very close to zero
         return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
     
     @staticmethod
     def delta(z, a , y):
         """ tells our network how to compute the output error. σ^l = a^l - y
+            Returns the erro delta from the output layer. Although the parametr z is not used in the function it is included in the method's 
+            parameters in order to make the interface consistent with the delta method for other cost classes.
         """
         return (a-y)
 
