5e21fbcc4952dfab5268a86d5df19770708262fb
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-06-29 21:08:54 +0800

    updated main with testcases

--- main.py
+++ main.py
@@ -23,9 +23,13 @@ net = network.Network([784, 30, 10])
 net.SGD(training_data , 30, 10, 3.0, test_data=test_data)
 
 
-#network2.py example
+#network2.py example1
 training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
 net2 =network2.Network([784, 30, 10], cost= network2.CrossEntropyCost)
 net2.large_weight_initializer()
 net2.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,monitor_evaluation_accuracy=True)
 
+#network2.py example2
+training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
+net2a = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True, monitor_training_cost=True)
