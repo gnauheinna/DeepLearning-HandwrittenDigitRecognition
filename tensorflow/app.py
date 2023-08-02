699b7fb1021ecc3f28b6c71efcc54b858024b754
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-08-02 21:42:28 +0800

    completed pygame

--- tensorflow/app.py
+++ tensorflow/app.py
@@ -5,21 +5,23 @@ from keras.models import load_model
 import cv2
 
 WINDOWSIZE = (640, 480)
-
+WINDOWSIZEX = 640
+WINDOWSIZEY = 480
+BOUNDRYINC = 5
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0,)
 RED =(255, 0, 0)
 
 IMAGESAVE = False
 
-MODEL = load_model("model.h5")
+MODEL = load_model("/Users/anniehuang/Desktop/DeepLearning-HandwrittenDigitRecognition/tensorflow/model.h5")
 
 LABELS = {0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"}
 
 # create application window
 pygame.init()
 
-FONT = pygame.font.Font("freesansbold.tff", 18)
+FONT = pygame.font.Font("freesansbold.ttf", 18)
 
 DISPLAYSURF = pygame.display.set_mode(WINDOWSIZE)
 
@@ -29,6 +31,10 @@ iswriting = False
 
 number_xcord = []
 number_ycord = []
+
+image_cnt =1
+
+PREDICT = True
 
 while True:
     for event in pygame.event.get():
@@ -47,4 +53,43 @@ while True:
             iswriting = True
 
 
+        if event.type == MOUSEBUTTONUP:
+            iswriting = False
 
+            if number_xcord and number_ycord:
+                number_xcord = sorted(number_xcord)
+                number_ycord = sorted(number_ycord)
+
+                rect_min_x, rect_max_x = max(number_xcord[0] - BOUNDRYINC, 0 ), min(WINDOWSIZEX, number_xcord[-1]+BOUNDRYINC )
+                rect_min_y, rect_max_y = max(number_ycord[0] - BOUNDRYINC, 0 ), min(number_ycord[-1]+BOUNDRYINC, WINDOWSIZEY)
+
+            number_xcord = []
+            number_ycord = []
+
+            img_arr = np.array(pygame.PixelArray(DISPLAYSURF))[rect_min_x:rect_max_x, rect_min_y:rect_max_y].T.astype(np.float32)
+
+            if IMAGESAVE:
+                cv2.imwrite("image.png")
+                image_cnt += 1
+
+            # incorporate python with ML
+            if PREDICT:
+                # resize the array representing drawn digit to 28*28 to fit the model 
+                image = cv2.resize(img_arr, (28, 28))
+                image = np.pad(image, (10, 10), 'constant', constant_values = 0)
+                # resizing the image and normalizing it
+                image = cv2.resize(image, (28, 28))/255
+
+                label = str(LABELS[np.argmax(MODEL.predict(image.reshape(1, 28, 28, 1)))])
+                print(np.argmax(MODEL.predict(image.reshape(1, 28, 28, 1))))
+                textSurface = FONT.render(label, True, RED, WHITE)
+                textRecObj = textSurface.get_rect()
+                textRecObj.left, textRecObj.bottom = rect_min_x, rect_max_y
+
+                DISPLAYSURF.blit(textSurface, textRecObj)
+
+            if event.type == KEYDOWN:
+                if event.unicode == "n":
+                    DISPLAYSURF.fill(BLACK)
+
+    pygame.display.update()
